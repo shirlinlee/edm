c5f0036e63a09204cfcf419eac4d0200394b1d50
Unknown <hn85412404@gmail.com>
2019-01-05 16:34:45 +0800

    nav done

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,6 +1,8 @@
 $(document)
-    .ready(function () {
-        $('.carousel').slick({slidesToShow: 1, dots: false, infinite: false});
-        $('.carousel_huge').slick({slidesToShow: 1, dots: true, infinite: false});
-
+    .ready(function() {
+        $('.carousel').slick({ slidesToShow: 1, dots: false, infinite: false });
+        $('.carousel_huge').slick({ slidesToShow: 1, dots: true, infinite: false, adaptiveHeight: true });
+        $('body').on('click', '.hamburger', function() {
+            $('.nav,.hamburger,.nav_bg').toggleClass('show');
+        })
     });
