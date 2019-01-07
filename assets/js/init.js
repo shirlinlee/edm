ab1619fb3244bfb8d1e31eb173a0eae83a8b917a
Unknown <hn85412404@gmail.com>
2019-01-07 13:18:01 +0800

    燈箱100% 後續調整80%

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,5 +4,13 @@ $(document)
         $('.carousel_huge').slick({ slidesToShow: 1, dots: true, infinite: false, adaptiveHeight: true });
         $('body').on('click', '.hamburger', function() {
             $('.nav,.hamburger,.nav_bg').toggleClass('show');
+        });
+        $('body').on('click', '.more', function() {
+            $('.lb,.nav_bg').toggleClass('show');
         })
+
+        if ($(window).innerWidth() <= 450) {
+            $('.mb_carousel').slick({ slidesToShow: 1, dots: false, infinite: false });
+
+        }
     });
