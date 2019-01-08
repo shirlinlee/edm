6ad372764736eeb6ffb5f6b381c7af45752558aa
Unknown <hn85412404@gmail.com>
2019-01-08 14:17:30 +0800

    debug

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,19 +1,32 @@
-var $body = (window.opera)
-    ? (document.compatMode == "CSS1Compat"
-        ? $('html')
-        : $('body'))
-    : $('html,body');
+var $body = (window.opera) ?
+    (document.compatMode == "CSS1Compat" ?
+        $('html') :
+        $('body')) :
+    $('html,body');
 
 $(document).ready(function () {
-    $('.carousel').slick({slidesToShow: 1, dots: false, infinite: false});
-    $('.carousel_huge').slick({slidesToShow: 1, dots: true, infinite: false, adaptiveHeight: true});
+    $('.carousel').slick({
+        slidesToShow: 1,
+        dots: false,
+        infinite: false
+    });
+    $('.carousel_huge').slick({
+        slidesToShow: 1,
+        dots: true,
+        infinite: false,
+        adaptiveHeight: true
+    });
 
     $('body').on('click', '.hamburger, .nav.show li', function () {
         $('.nav,.hamburger,.nav_bg').toggleClass('show');
     });
 
-    $('body').on('click', '.more, .close', function () {
-        $('.lb,.nav_bg').toggleClass('show show_lb');
+    $('body').on('click', '.close', function () {
+        $('.lb,.nav_bg').removeClass('show show_lb');
+    })
+
+    $('body').on('click', '.article', function () {
+        if ($(this).find('.btn').is(':visible')) $('.lb,.nav_bg').addClass('show show_lb');
     })
 
     // if ($(window).innerWidth() <= 450) {
@@ -21,17 +34,15 @@ $(document).ready(function () {
         settings: "unslick",
         dots: true,
         infinite: false,
-        responsive: [
-            {
-                breakpoint: 2560,
-                settings: "unslick"
-            }, {
-                breakpoint: 450,
-                settings: {
-                    slidesToShow: 1
-                }
+        responsive: [{
+            breakpoint: 2560,
+            settings: "unslick"
+        }, {
+            breakpoint: 450,
+            settings: {
+                slidesToShow: 1
             }
-        ]
+        }]
     });
 
     $('#banner').on('click', '.arrow', function () {
@@ -76,7 +87,40 @@ $(document).ready(function () {
                 isScroll = false;
             }, 2000)
         }
+        //是版型三
+        if ($('.parallax').length === 1 && $(window).innerWidth() > 450) {
+            var scroll = $(window).scrollTop();
+            var windowH = $(window).innerHeight();
+            var point = (scroll - windowH) / 1 - 190;
+
+            if (point >= 500) {
+                point = 500;
+            }
+
+            if (point <= -30) {
+                point = -30;
+            }
+
+            // console.log(point);
+
+            $('.parallax').css({
+                'transform': 'translate(0,' + point + 'px)'
+            })
+
+        }
     });
+
+
+    if (navigator.msSaveBlob) {
+        console.log('ie');
+        return navigator.msSaveBlob(blob, fileName);
+    }
+
+
+
+
+
+
 
     // } else {}
 });
